7b96d5da9f6ef293791e62ca04488cd283a4a837
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-08-19 09:39:20 -0400

    Update Carrusel.jsx

--- src/Components/Carrusel/Carrusel.jsx
+++ src/Components/Carrusel/Carrusel.jsx
@@ -4,7 +4,7 @@ import CajaInf from '../../../src/Components/Home/CajaInf/CajaInf'
 
 function Carrusel() {
     return (
-        <Carousel className="Container">
+        <Carousel style={{ background: '#808080' }}>
 
             <Carousel.Item>
                 <div class="d-flex h-100 align-items-center justify-content-center">
@@ -12,7 +12,7 @@ function Carrusel() {
 
                         imagen=''
                         alt=''
-                        name='primero'
+                        name='Primero'
                         text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit incidunt ad deleniti eos vitae porro sequi alias, deserunt nobis voluptatem, soluta minima, molestias tempora a illo odio eveniet eligendi aspernatur fugiat eius. Numquam ea omnis, sapiente tempora temporibus dicta similique! Ipsum repellendus quas obcaecati id quae. Hic est aliquid sequi aspernatur, ducimus inventore dolor, ut quod error, mollitia accusamus veritatis facere facilis. Magnam doloremque animi aspernatur soluta omnis explicabo asperiores nostrum a nemo quis harum, suscipit magni vel aliquid eaque ipsum repudiandae? Voluptas praesentium doloremque sit atque sed veniam alias in voluptatibus, quod voluptatem, neque rem cumque quo quaerat culpa?'
                     />
 
@@ -31,7 +31,7 @@ function Carrusel() {
                     <CajaInf
                         imagen=''
                         alt=''
-                        name='segundo'
+                        name='Segundo'
                         text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit incidunt ad deleniti eos vitae porro sequi alias, deserunt nobis voluptatem, soluta minima, molestias tempora a illo odio eveniet eligendi aspernatur fugiat eius. Numquam ea omnis, sapiente tempora temporibus dicta similique! Ipsum repellendus quas obcaecati id quae. Hic est aliquid sequi aspernatur, ducimus inventore dolor, ut quod error, mollitia accusamus veritatis facere facilis. Magnam doloremque animi aspernatur soluta omnis explicabo asperiores nostrum a nemo quis harum, suscipit magni vel aliquid eaque ipsum repudiandae? Voluptas praesentium doloremque sit atque sed veniam alias in voluptatibus, quod voluptatem, neque rem cumque quo quaerat culpa?'
                     />
 
@@ -49,7 +49,46 @@ function Carrusel() {
                     <CajaInf
                         imagen=''
                         alt=''
-                        name='tercero'
+                        name='Tercero'
+                        text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit incidunt ad deleniti eos vitae porro sequi alias, deserunt nobis voluptatem, soluta minima, molestias tempora a illo odio eveniet eligendi aspernatur fugiat eius. Numquam ea omnis, sapiente tempora temporibus dicta similique! Ipsum repellendus quas obcaecati id quae. Hic est aliquid sequi aspernatur, ducimus inventore dolor, ut quod error, mollitia accusamus veritatis facere facilis. Magnam doloremque animi aspernatur soluta omnis explicabo asperiores nostrum a nemo quis harum, suscipit magni vel aliquid eaque ipsum repudiandae? Voluptas praesentium doloremque sit atque sed veniam alias in voluptatibus, quod voluptatem, neque rem cumque quo quaerat culpa?'
+                    />
+
+                    {/*
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+                */}
+                </div>
+            </Carousel.Item>
+            
+            <Carousel.Item>
+                <div class="d-flex h-100 align-items-center justify-content-center">
+                    <CajaInf
+                        imagen=''
+                        alt=''
+                        name='Cuarto'
+                        text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit incidunt ad deleniti eos vitae porro sequi alias, deserunt nobis voluptatem, soluta minima, molestias tempora a illo odio eveniet eligendi aspernatur fugiat eius. Numquam ea omnis, sapiente tempora temporibus dicta similique! Ipsum repellendus quas obcaecati id quae. Hic est aliquid sequi aspernatur, ducimus inventore dolor, ut quod error, mollitia accusamus veritatis facere facilis. Magnam doloremque animi aspernatur soluta omnis explicabo asperiores nostrum a nemo quis harum, suscipit magni vel aliquid eaque ipsum repudiandae? Voluptas praesentium doloremque sit atque sed veniam alias in voluptatibus, quod voluptatem, neque rem cumque quo quaerat culpa?'
+                    />
+
+                    {/*
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+                */}
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div class="d-flex h-100 align-items-center justify-content-center">
+                    <CajaInf
+                        imagen=''
+                        alt=''
+                        name='Quinto'
                         text='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit incidunt ad deleniti eos vitae porro sequi alias, deserunt nobis voluptatem, soluta minima, molestias tempora a illo odio eveniet eligendi aspernatur fugiat eius. Numquam ea omnis, sapiente tempora temporibus dicta similique! Ipsum repellendus quas obcaecati id quae. Hic est aliquid sequi aspernatur, ducimus inventore dolor, ut quod error, mollitia accusamus veritatis facere facilis. Magnam doloremque animi aspernatur soluta omnis explicabo asperiores nostrum a nemo quis harum, suscipit magni vel aliquid eaque ipsum repudiandae? Voluptas praesentium doloremque sit atque sed veniam alias in voluptatibus, quod voluptatem, neque rem cumque quo quaerat culpa?'
                     />
 
