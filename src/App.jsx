a34905117824c12697c8518b9e0df38e205f995c
Sander Peguero <sanderpeguero.21@gmail.com>
2022-08-20 21:31:20 -0400

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 // import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Components/Home/Home.jsx'
+import Navbar from './Components/Navbar/Navbar.jsx'
+import Footer from './Components/Home/Footer/Footer.jsx'
+import Body from './Components/Home/Body.jsx'
 import './App.css'
 
 
@@ -12,7 +16,23 @@ import './App.css'
 function App() {
 
   return (
-    <Home/>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh'
+    }}>
+      <BrowserRouter>
+        <header>
+          <Navbar/>
+        </header>
+        <main>
+          <Routes>
+            <Route exact path='/' element={<Home/>}></Route>
+          </Routes>
+        </main>
+        <Footer/>
+      </BrowserRouter>
+    </div>
   )
 }
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,10 @@
-import Navbar from '../Navbar/Navbar.jsx'
 import Body from "./Body.jsx"
-import Footer from "../Home/Footer/Footer.jsx"
 
 //Pagina de inicio
 const Home = () => {
 
     return(
-        <div>
-            <Navbar/>
-            <Body/>
-            <Footer/>
-        </div>
+        <Body/>
     ) 
         
 }
